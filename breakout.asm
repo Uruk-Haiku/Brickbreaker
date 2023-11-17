b403472366a62083e4fc3472843c857f420b180a
Uruk-Haiku <59447359+Uruk-Haiku@users.noreply.github.com>
2023-11-17 14:32:45 -0500

    Update breakout.asm

--- breakout.asm
+++ breakout.asm
@@ -1,8 +1,3 @@
-################ CSC258H1F Fall 2022 Assembly Final Project ##################
-# This file contains our implementation of Breakout.
-#
-# Student 1: Omar, Alshawa, 1007198186
-# Student 2: Benjamin Panet, 1008121417
 ######################## Bitmap Display Configuration ########################
 # - Unit width in pixels:       16
 # - Unit height in pixels:      16
